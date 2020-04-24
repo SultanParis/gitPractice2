63e05b2b66ed7127e87afa8ef5e56272aab2a524
SultanParis <kzmotors1@gmail.com>
2020-04-24 16:29:51 -0400

    Update allGood.java

--- src/day_1/allGood.java
+++ src/day_1/allGood.java
@@ -4,5 +4,7 @@ public class allGood {
     public static void main(String[] args) {
 
         System.out.println("Everything is going to be good!");
+        
+        // this line was edited from the Github
     }
 }
