e1b8e2a20ca4dbf4ac2b4647a8feaeef4b67875b
SultanParis <kzmotors1@gmail.com>
2020-04-28 15:49:49 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,5 +4,6 @@ public class HappyBirthday {
     public static void main(String[] args) {
 
         System.out.println("Happy Birthday B18!");
+        System.out.println("Thank you Asiya!");  // this line is added from the GitHub
     }
 }
